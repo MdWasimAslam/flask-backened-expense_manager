6575adaae0428ba23adb446844f4a560e29c63c4
Md Wasim Aslam <wasimaslam2897@gmail.com>
2024-07-22 15:39:35 +0530

    fix check

--- api/index.py
+++ api/index.py
@@ -1,11 +1,11 @@
 from flask import Flask
-# from Controllers.user_controller import user
+from Controllers.user_controller import user
 # from Controllers.products_controller import products
 
 
 app = Flask(__name__)
 
-# app.register_blueprint(user, url_prefix='/user')
+app.register_blueprint(user, url_prefix='/user')
 # app.register_blueprint(products, url_prefix='/products')
 
 
@@ -14,5 +14,5 @@ def home():
     return 'Home'
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
